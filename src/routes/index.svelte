b824c877e56c8423d44aa275b6cd35d236befcd6
toolongformore <freejailsymphonic@gmail.com>
2021-09-29 08:12:28 +0200

    Add example code for await-block

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  async function getRandomNumber() {
+    await new Promise((resolve) => {
+      setTimeout(() => resolve(), 1000);
+    });
+    const randomNumber = Math.random();
+
+    if (randomNumber > 0.5) {
+      return randomNumber;
+    } else {
+      throw new Error(randomNumber.toString());
+    }
+  }
+
+  let promise = getRandomNumber();
+
+  function handleClick() {
+    promise = getRandomNumber();
+  }
+</script>
+
+<button on:click={handleClick}> generate random number </button>
+
+{#await promise}
+  <p>...waiting</p>
+{:then number}
+  <p>The number is {number}</p>
+{:catch error}
+  <p style="color: red">{error.message}</p>
+{/await}
