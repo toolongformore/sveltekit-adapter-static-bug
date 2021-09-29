b7ea3f9f56d70594815fa7e31470752f3cdef511
toolongformore <freejailsymphonic@gmail.com>
2021-09-29 08:22:12 +0200

    Add deterministic example

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,30 +1,32 @@
-<script>
-  async function getRandomNumber() {
-    await new Promise((resolve) => {
-      setTimeout(() => resolve(), 1000);
-    });
-    const randomNumber = Math.random();
+<script lang="ts">
+	async function getRandomNumber(throwError: boolean) {
+		if (throwError) {
+			throw new Error('error');
+		} else {
+			return 'ok';
+		}
+	}
 
-    if (randomNumber > 0.5) {
-      return randomNumber;
-    } else {
-      throw new Error(randomNumber.toString());
-    }
-  }
+	// with this line, everything works fine
+	let promise = getRandomNumber(false);
+	// with this line, dev server crashes on start
+	// let promise = getRandomNumber(true);
 
-  let promise = getRandomNumber();
-
-  function handleClick() {
-    promise = getRandomNumber();
-  }
+	function getResponse() {
+		promise = getRandomNumber(false);
+	}
+	function throwError() {
+		promise = getRandomNumber(true);
+	}
 </script>
 
-<button on:click={handleClick}> generate random number </button>
+<button on:click={getResponse}> get response </button>
+<button on:click={throwError}> throw error </button>
 
 {#await promise}
-  <p>...waiting</p>
+	<p>...waiting</p>
 {:then number}
-  <p>The number is {number}</p>
+	<p>The number is {number}</p>
 {:catch error}
-  <p style="color: red">{error.message}</p>
+	<p style="color: red">{error.message}</p>
 {/await}
